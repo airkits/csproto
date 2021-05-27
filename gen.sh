6060e35b74f7ae3c0f697efa3d6fcbecd4e0e206
admin <>
2021-05-28 00:08:19 +0800

    update frame proto

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 basepath=$(cd `dirname $0`; pwd)
 echo $basepath
-#protoc -I $basepath --go_out=$basepath $basepath/c2s.proto
-protoc --go_out=plugins=grpc:./c2s --go_opt=paths=source_relative $basepath/c2s/c2s.proto
+protoc -I $basepath --go_out=./c2s $basepath/c2s/c2s.proto
+#protoc --go_out=./c2s --go_opt=paths=source_relative $basepath/c2s/c2s.proto
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/airkits/csproto
 
 go 1.15
 
-require (
-	github.com/golang/protobuf v1.5.2
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/protobuf v1.26.0
-)
+require google.golang.org/protobuf v1.26.0
